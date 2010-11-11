921773b727c75c4056114249354bcdc6cdbe8309
1007lq <1007lq@gmail.com>
2010-11-11 09:47:48 +0800

    delete one line in dev

--- hello.c
+++ hello.c
@@ -4,7 +4,6 @@
 int main(int argc, const char *argv[])
 {
 	printf("add one line in branch master from web\n");
-	printf("add one line in branch master\n");
 	printf("add one line in branch dev\n");
 
 	return 0;
