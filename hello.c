f4a8b3a6a2ee3bbce7a39b2dd3b5a70c6826476b
1007lq <1007lq@gmail.com>
2010-11-11 09:41:01 +0800

    dev

--- hello.c
+++ hello.c
@@ -6,6 +6,7 @@ int main(int argc, const char *argv[])
 	printf("add one line in branch master from web\n");
 	printf("add one line in branch master\n");
 	printf("add one line in branch master========--------\n");
+	printf("add one line in branch dev\n");
 
 	return 0;
 }
